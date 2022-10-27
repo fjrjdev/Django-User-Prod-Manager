507acae757edb4cea2ff101f1f20db720f7745db
fjrjdev <thedromemafia@gmail.com>
2022-10-27 18:30:57 -0300

    refactor : db settings config

--- project/settings.py
+++ project/settings.py
@@ -124,9 +124,8 @@ DATABASES = {
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
-    db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-    DATABASES['default'].update(db_from_env)
+    db = dj_database_url.config(default=DATABASE_URL)
+    DATABASES["default"].update(db)
 
     DEBUG = False
 
